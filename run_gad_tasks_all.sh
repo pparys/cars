e5bff6869d747bec67b6983875839ed7c4e5c87f
Paweł Parys <parys@mimuw.edu.pl>
2025-08-30 07:40:21 -0700

    script

--- run_gad_tasks_all.sh
+++ run_gad_tasks_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup python run_gad_tasks.py --split BV4 --styles=rs,cftrs >log-BV4.txt &
-nohup python run_gad_tasks.py --split CP --styles=rs,cftrs >log-CP.txt &
-nohup python run_gad_tasks.py --split SLIA --styles=rs,cftrs >log-SLIA.txt &
+CUDA_VISIBLE_DEVICES=2 nohup python run_gad_tasks.py --split BV4 --styles=rs,cftrs >log-BV4.txt &
+CUDA_VISIBLE_DEVICES=3 nohup python run_gad_tasks.py --split CP --styles=rs,cftrs >log-CP.txt &
+CUDA_VISIBLE_DEVICES=4 nohup python run_gad_tasks.py --split SLIA --styles=rs,cftrs >log-SLIA.txt &
