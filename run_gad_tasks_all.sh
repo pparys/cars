1a85534d33ea6026c8d190e76b5ff37df4eda9cd
Paweł Parys <parys@mimuw.edu.pl>
2025-08-30 07:39:36 -0700

    script

--- run_gad_tasks_all.sh
+++ run_gad_tasks_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python run_gad_tasks.py --split BV4
-python run_gad_tasks.py --split CP
-python run_gad_tasks.py --split SLIA
+nohup python run_gad_tasks.py --split BV4 --styles=rs,cftrs >log-BV4.txt &
+nohup python run_gad_tasks.py --split CP --styles=rs,cftrs >log-CP.txt &
+nohup python run_gad_tasks.py --split SLIA --styles=rs,cftrs >log-SLIA.txt &
