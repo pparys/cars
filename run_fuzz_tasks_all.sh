ef5848769fac48ce34796736adfd222fe41fc2c0
Paweł Parys <parys@mimuw.edu.pl>
2025-09-16 16:21:55 -0700

    run_fuzzing

--- run_fuzz_tasks_all.sh
+++ run_fuzz_tasks_all.sh
@@ -5,9 +5,9 @@ if [ "$#" -ne 1 ]; then
 	exit 1
 fi
 
-python run_task.py json.lark generate_json.txt $1
-python run_task.py json.lark generate_json_with_grammar.txt $1
-python run_task.py sql.lark generate_sql.txt $1
-python run_task.py sql.lark generate_sql_with_grammar.txt $1
-python run_task.py xml.lark generate_xml.txt $1
-python run_task.py xml.lark generate_xml_with_grammar.txt $1
+python run_task.py datasets/fuzzing/json.lark datasets/fuzzing/generate_json.txt $1
+python run_task.py datasets/fuzzing/json.lark datasets/fuzzing/generate_json_with_grammar.txt $1
+python run_task.py datasets/fuzzing/sql.lark datasets/fuzzing/generate_sql.txt $1
+python run_task.py datasets/fuzzing/sql.lark datasets/fuzzing/generate_sql_with_grammar.txt $1
+python run_task.py datasets/fuzzing/xml.lark datasets/fuzzing/generate_xml.txt $1
+python run_task.py datasets/fuzzing/xml.lark datasets/fuzzing/generate_xml_with_grammar.txt $1
