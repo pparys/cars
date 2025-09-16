0767c11aa4b9da9501873869ea453006e622d62f
Paweł Parys <parys@mimuw.edu.pl>
2025-09-16 16:14:31 -0700

    fuzzing prompts

--- display_output.py
+++ display_output.py
@@ -4,11 +4,16 @@ def display_output(path : str):
     with open(path, "r") as f:
         run_data = json.load(f)
     steps = run_data["steps"]
+    multiline = any(any('\n' in word for word in a['tokens']) for a in steps)
+    print(multiline)
+
     for a in steps:
         for b in a['tokens']:
-            print(b, end="")
+            if b != '<|eot_id|>':
+                print(b, end="")
         print()
-        print("################")
+        if multiline:
+            print("\n################")
         
 
 if __name__ == "__main__":
