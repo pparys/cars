ef487b1656f00044c9210b55cc315e25d8504b80
Paweł Parys <parys@mimuw.edu.pl>
2025-09-16 16:17:22 -0700

    run_fuzzing

--- run_fuzz_tasks_all.sh
+++ run_fuzz_tasks_all.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-python run_fuzz_tasks.py --benchmark xml
-python run_fuzz_tasks.py --benchmark sql
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 sampling_style"
+	exit 1
+fi
+
+python run_task.py json.lark generate_json.txt $1
+python run_task.py json.lark generate_json_with_grammar.txt $1
+python run_task.py sql.lark generate_sql.txt $1
+python run_task.py sql.lark generate_sql_with_grammar.txt $1
+python run_task.py xml.lark generate_xml.txt $1
+python run_task.py xml.lark generate_xml_with_grammar.txt $1
