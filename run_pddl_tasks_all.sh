a02e33da7af58f51e40f485017fcd7de0486d2d2
emmanuel <fanayagonzalez@ucsd.edu>
2025-09-20 15:35:28 -0700

    update run script

--- run_pddl_tasks_all.sh
+++ run_pddl_tasks_all.sh
@@ -8,11 +8,20 @@ fi
 for run in {1..2}; do    
     python run_task.py experiments/PDDL/domains/blocks/grammar.lark experiments/PDDL/prompts/blocks/task04_prompt.txt $1 $2
     python run_task.py experiments/PDDL/domains/blocks/grammar.lark experiments/PDDL/prompts/blocks/task05_prompt.txt $1 $2
+    python run_task.py experiments/PDDL/domains/blocks/grammar.lark experiments/PDDL/prompts/blocks/task08_prompt.txt $1 $2
+    python run_task.py experiments/PDDL/domains/blocks/grammar.lark experiments/PDDL/prompts/blocks/task09_prompt.txt $1 $2
+
+
     python run_task.py experiments/PDDL/domains/depot/grammar.lark experiments/PDDL/prompts/depot/task02_prompt.txt $1 $2
     python run_task.py experiments/PDDL/domains/depot/grammar.lark experiments/PDDL/prompts/depot/task04_prompt.txt $1 $2
+    python run_task.py experiments/PDDL/domains/depot/grammar.lark experiments/PDDL/prompts/depot/task07_prompt.txt $1 $2
+    python run_task.py experiments/PDDL/domains/depot/grammar.lark experiments/PDDL/prompts/depot/task08_prompt.txt $1 $2
+
     python run_task.py experiments/PDDL/domains/satellite/grammar.lark experiments/PDDL/prompts/satellite/task03_prompt.txt $1 $2
     python run_task.py experiments/PDDL/domains/satellite/grammar.lark experiments/PDDL/prompts/satellite/task05_prompt.txt $1 $2
-    
+    python run_task.py experiments/PDDL/domains/satellite/grammar.lark experiments/PDDL/prompts/satellite/task07_prompt.txt $1 $2
+    python run_task.py experiments/PDDL/domains/satellite/grammar.lark experiments/PDDL/prompts/satellite/task08_prompt.txt $1 $2
+
     echo "Completed run $run"
     echo ""
 done
